61c166aa13bb4acde7c3063936fb0202abcf219d
Valerie Tan <valerietanhx@gmail.com>
2024-07-23 21:31:47 +0800

    add toggle behaviour to item card

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,19 +1,19 @@
-import { PropsWithChildren } from "react";
+import { useState, PropsWithChildren } from "react";
 import Card from "../Card/Card";
 import styles from "./itemCard.module.css";
 import IconButton from "../IconButton/IconButton";
-import { faCheck, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faTrashCan,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface ItemCardProps {
   date: Date;
 }
 
 function ItemCard({ date, children }: PropsWithChildren<ItemCardProps>) {
-  const LIMIT = 30;
-
-  const currentDate = new Date();
-  const diffTime = Math.abs(currentDate.getTime() - date.getTime());
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const [isDone, setIsDone] = useState<boolean>(false);
 
   const options: Intl.DateTimeFormatOptions = {
     weekday: "long",
@@ -22,21 +22,41 @@ function ItemCard({ date, children }: PropsWithChildren<ItemCardProps>) {
     day: "numeric",
   };
 
+  const limit = 30;
+  const currentDate = new Date();
+  const diffTime = Math.abs(currentDate.getTime() - date.getTime());
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  const toggleDone = () => {
+    setIsDone(!isDone);
+  };
+
+  const handleDelete = () => {
+    console.log("item deleted");
+  };
+
   return (
-    <Card status={diffDays <= 10 ? "fresh" : diffDays <= 20 ? "stale" : "aged"}>
+    <Card
+      status={
+        isDone
+          ? "neutral"
+          : diffDays <= 10
+          ? "fresh"
+          : diffDays <= 20
+          ? "stale"
+          : "aged"
+      }
+    >
       <div className={styles.content}>{children}</div>
       <hr></hr>
       <div className={styles.footer}>
         <div className={styles.metadata}>
           Added {date.toLocaleDateString(navigator.language, options)}. Expires
-          in {LIMIT - diffDays} days.
+          in {limit - diffDays} days.
         </div>
         <div className={styles.buttons}>
-          <IconButton icon={faCheck} onClick={() => console.log("item done")} />
-          <IconButton
-            icon={faTrashCan}
-            onClick={() => console.log("item deleted")}
-          />
+          <IconButton icon={isDone ? faXmark : faCheck} onClick={toggleDone} />
+          <IconButton icon={faTrashCan} onClick={handleDelete} />
         </div>
       </div>
     </Card>
