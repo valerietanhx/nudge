97f1d7dc4703ea63cee9109805496ff01425c18e
Valerie Tan <valerietanhx@gmail.com>
2024-07-24 23:25:14 +0800

    wrap long urls and text

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,9 @@ function App() {
             {/*
             TODO:
             - handle file, haven't managed to get it working
-            - fix url and text overflow
             */}
-            {value.url && <a href={value.url}>{value.url}</a>}
-            {value.text && <p>{value.text}</p>}
+            {value.url && <a href={value.url} className="wrap">{value.url}</a>}
+            {value.text && <p className="wrap">{value.text}</p>}
           </ItemCard>
         ))}
     </main>
